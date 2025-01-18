9e6d4b8aeeb3e15a242114cced4921363b992e07
ann242004 <ann242004@gmail.com>
2025-01-18 17:11:27 +0530

    logo changed

--- hagana/lib/pages/Welcomescren.dart
+++ hagana/lib/pages/Welcomescren.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hagana/pages/CommunityPage.dart';
-// import 'package:hagana/pages/CommunityPage.dart';
 import 'package:hagana/pages/HomePage.dart';
 import 'package:hagana/pages/LoginPage.dart';
 import 'package:hagana/pages/SignUp.dart';
-// import 'package:hagana/pages/Homepage.dart';
-// import '../error_boundary.dart';
 
 class Welcomescreen extends StatefulWidget {
   const Welcomescreen({super.key});
@@ -24,7 +21,7 @@ class _WelcomescreenState extends State<Welcomescreen> {
     Future.delayed(const Duration(seconds: 2), () {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) =>  HomeScreen()),
+        MaterialPageRoute(builder: (context) => HomeScreen()),
       );
     });
   }
@@ -33,25 +30,15 @@ class _WelcomescreenState extends State<Welcomescreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Container(
-          decoration: const BoxDecoration(
-            color: Color.fromRGBO(222, 225, 230, 1.0),
-          ),
-          child: LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
-              return Center(
-                child: Text(
-                  "Hagana",
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.allura(
-                    fontWeight: FontWeight.w400,
-                    color: const Color.fromRGBO(23, 26, 31, 1.0),
-                    decoration: TextDecoration.none,
-                    fontSize: 47.0,
-                  ),
-                ),
-              );
-            },
+        decoration: const BoxDecoration(
+          color: Color(0xFF051650), // New background color #051650
+        ),
+        child: Center(
+          child: Image.asset(
+            'assets/images/hagana_logo.png',
+            width: MediaQuery.of(context).size.width *
+                1.0, // Increased from 0.8 to 0.9 (90% of screen width)
+            fit: BoxFit.contain,
           ),
         ),
       ),
