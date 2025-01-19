508dc18e7b8e28245b8d10e8515fd6492eeb7107
Alwin-Sajan <alwinsajan225@gmail.com>
2025-01-19 09:46:16 +0530

    add animation Welcome Scrren

--- hagana/lib/pages/Welcomescren.dart
+++ hagana/lib/pages/Welcomescren.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:hagana/pages/CommunityPage.dart';
-import 'package:hagana/pages/HomePage.dart';
-import 'package:hagana/pages/LoginPage.dart';
-import 'package:hagana/pages/SignUp.dart';
-import 'package:hagana/init_setup.dart';
 import 'package:hagana/pages/HaganaHomeScreen.dart';
+import 'package:hagana/pages/HomePage.dart';
+import 'package:hagana/init_setup.dart'; // Assuming this is where SecureStorage is defined.
 
 class Welcomescreen extends StatefulWidget {
   const Welcomescreen({super.key});
@@ -14,12 +10,37 @@ class Welcomescreen extends StatefulWidget {
   State<Welcomescreen> createState() => _WelcomescreenState();
 }
 
-class _WelcomescreenState extends State<Welcomescreen> {
+class _WelcomescreenState extends State<Welcomescreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _slideAnimation;
+  late Animation<double> _fadeAnimation;
   final SecureStorage _secureStorage = SecureStorage();
 
   @override
   void initState() {
     super.initState();
+
+    // Initialize the animation controller and the animations
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2), // Set animation duration
+      vsync: this,
+    )..forward(); // Start the animation immediately
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(-1.0, 0.0), // Start off the left side of the screen
+      end: Offset.zero, // End at the center
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut, // Ease-in-out transition
+    ));
+
+    _fadeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut, // Ease-in-out transition
+    ));
 
     // Check if user is already logged in
     _checkLoginStatus();
@@ -33,27 +54,44 @@ class _WelcomescreenState extends State<Welcomescreen> {
         context,
         MaterialPageRoute(
           builder: (context) =>
-              isLoggedIn ? const HaganaHomeScreen() : const HomeScreen(),
+          isLoggedIn ? const HaganaHomeScreen() : const HomeScreen(),
         ),
       );
     });
   }
 
   @override
+  void dispose() {
+    // Dispose the animation controller when done
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          color: Color(0xFF051650), // Background color #051650
-        ),
-        child: Center(
-          child: Image.asset(
-            'assets/images/hagana_logo.png',
-            width:
-                MediaQuery.of(context).size.width * 0.9, // 90% of screen width
-            fit: BoxFit.contain,
-          ),
-        ),
+      body: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) {
+          return SlideTransition(
+            position: _slideAnimation,
+            child: FadeTransition(
+              opacity: _fadeAnimation,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Color(0xFF051650), // Background color #051650
+                ),
+                child: Center(
+                  child: Image.asset(
+                    'assets/images/hagana_logo.png',
+                    width: MediaQuery.of(context).size.width * 0.9, // 90% of screen width
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
